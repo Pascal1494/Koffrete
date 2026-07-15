a8cafab4c1c9676b3e84acfbffd24ae697ebfe99
Pascal1494 <discokilly@laposte.net>
2026-07-16 00:50:34 +0200

    refactor: remove user and loans relations from Media entity

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -3,14 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\MediaRepository;
-use App\Validator\AssertQuota;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MediaRepository::class)]
 #[ORM\InheritanceType('JOINED')]
-#[AssertQuota]
 #[ORM\DiscriminatorColumn(name: 'discr', type: 'string')]
 #[ORM\DiscriminatorMap([
     'book' => Book::class,
@@ -29,20 +25,9 @@ abstract class Media
     #[ORM\Column]
     private ?\DateTimeImmutable $createdAt = null;
 
-    #[ORM\ManyToOne(targetEntity: User::class)]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?User $user = null;
-
-    /**
-     * @var Collection<int, Loan>
-     */
-    #[ORM\OneToMany(mappedBy: 'media', targetEntity: Loan::class, cascade: ['all'], orphanRemoval: true)]
-    private Collection $loans;
-
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
-        $this->loans = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -70,48 +55,6 @@ abstract class Media
     public function setCreatedAt(\DateTimeImmutable $createdAt): static
     {
         $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
-
-    public function setUser(?User $user): static
-    {
-        $this->user = $user;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Loan>
-     */
-    public function getLoans(): Collection
-    {
-        return $this->loans;
-    }
-
-    public function addLoan(Loan $loan): static
-    {
-        if (!$this->loans->contains($loan)) {
-            $this->loans->add($loan);
-            $loan->setMedia($this);
-        }
-
-        return $this;
-    }
-
-    public function removeLoan(Loan $loan): static
-    {
-        if ($this->loans->removeElement($loan)) {
-            // set the owning side to null (unless already changed)
-            if ($loan->getMedia() === $this) {
-                $loan->setMedia(null);
-            }
-        }
 
         return $this;
     }
